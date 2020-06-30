0aa6221a891c000a6c8609feb548232461bcec12
gpoon626 <gpoon626@gmail.com>
2020-06-30 13:09:36 -0400

    Front-end auth complete

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,16 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-
+const userAction = require('./auth/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   // your JS code goes here
+  $('#signup-form').on('submit', userAction.onSignUp)
+  $('#sign-in-form').on('submit', userAction.onSignIn)
+  $('#change-password-form').on('submit', userAction.onChangePassword)
+  $('#sign-out').on('click', userAction.onSignOut)
+
+  $('#change-password').hide()
 })
